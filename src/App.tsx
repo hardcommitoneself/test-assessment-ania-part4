e0626146af358a40827f9c8f6ff3ede37bbc40b9
hardcommitoneself <hardcommitoneself@gmail.com>
2022-12-12 02:56:18 -0800

    feat: complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Col, Row, Typography, Layout, Card, Input, Checkbox, Button } from 'antd';
+import { UserOutlined, MailOutlined, LockOutlined } from '@ant-design/icons';
 import './App.css';
+
+const { Title, Link } = Typography;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ maxWidth: 1360, margin: '0 auto', marginTop: 40 }}>
+      <Card style={{ borderRadius: 20, padding: '80px' }}>
+        <Row>
+          {/* left */}
+          <Col span={12}>
+            <Row style={{ gap: 40, flexDirection: 'column', padding: '0 40px' }}>
+              {/* title */}
+              <Title style={{ fontWeight: 700 }}>Sign Up</Title>
+
+              {/* fields */}
+              <Row style={{ flexDirection: 'column', gap: 20 }}>
+                <Input
+                  placeholder="Your name"
+                  prefix={<UserOutlined style={{ marginRight: 10 }} />}
+                  style={{ borderTop: 'none', borderLeft: 'none', borderRight: 'none', borderRadius: '0', boxShadow: 'none' }}
+                />
+                <Input
+                  placeholder="Your email"
+                  type='email'
+                  prefix={<MailOutlined style={{ marginRight: 10 }} />}
+                  style={{ borderTop: 'none', borderLeft: 'none', borderRight: 'none', borderRadius: '0', boxShadow: 'none' }}
+                />
+                <Input
+                  placeholder="Your password"
+                  type='password'
+                  prefix={<LockOutlined style={{ marginRight: 10 }} />}
+                  style={{ borderTop: 'none', borderLeft: 'none', borderRight: 'none', borderRadius: '0', boxShadow: 'none' }}
+                />
+                <Input
+                  placeholder="Repeat your password"
+                  type='password'
+                  prefix={<LockOutlined style={{ marginRight: 10 }} />}
+                  style={{ borderTop: 'none', borderLeft: 'none', borderRight: 'none', borderRadius: '0', boxShadow: 'none' }}
+                />
+                <Checkbox style={{ margin: '0 10px' }}>I agree all statements in <Link>Terms of Service</Link> </Checkbox>
+              </Row>
+
+              <Button type="primary" style={{ width: 'fit-content', padding: '0 40px', height: '60px' }}>Register</Button>
+            </Row>
+          </Col>
+
+          {/* right */}
+          <Col span={12} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+            <Row style={{ flexDirection: 'column', gap: 10 }}>
+              <img src='/images/bg.jpg' alt='bg' style={{ width: '100%', height: 'auto' }} />
+
+              <Link style={{ textAlign: 'center', textDecoration: 'underline' }}>I am already memeber</Link>
+            </Row>
+          </Col>
+        </Row>
+      </Card>
+    </Layout>
   );
 }
 
